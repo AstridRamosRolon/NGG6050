5e11d772152f06a94bee2a5f8f41d8a0ec879173
Astrid <126601302+AstridRamosRolon@users.noreply.github.com>
2023-09-05 13:19:06 -0400

    HW1

--- Freq_vs_Bayse_HW.m
+++ Freq_vs_Bayse_HW.m
@@ -1,108 +1,62 @@
 %% Exercise #1: If someone gets a positive test, is it "statistically significant" at the p<0.05 level? Why or why not?
 
-%% Frequentist approach:
-
-    % If the p-value is p<0.05, it suggests that the probability of getting a positive test 
-    % result when the person is not infected (i.e., a false positive) is less than 5%. If the 
-    % p-value is p≥0.05, it suggests that there is not enough evidence to conclude that the 
-    % positive test result is statistically significant. In other words, it doesn't strongly 
-    % support the claim that the person is infected. The p-value alone doesn't tell you the 
-    % probability of being infected; it tells you the probability of observing the test result
-    % if the null hypothesis were true.
-
-clc; clear;
-    % Define parameters:
-    n_sim = 10000; % number of simulations
-    n_test = 1000; % number of tests per simulation
-    p = 0.02; % prevalence of desease
-    false_pos = 0.05; % false positive rate
-
-    % Initialize variable counting number of positive tests:
-    n_pos = 0;
-
-    % Simulate:
-    for i = 1:n_sim
-        % Generate random numbers for each test:
-        test = rand(n_test,1) < p + (1 - p) * false_pos;
-        % Check if any test is positive:
-        if any(test)
-            n_pos = n_pos + 1;
-        end
-    end
-
-    % Calculate p-value:
-    p_value = n_pos / n_sim;
-
-    % Check if p-value is less than 0.05:
-    if p_value < 0.05
-        fprintf('The positive test result is statistically significant (p = %.4f < 0.05).\n', p_value);
-    else
-        fprintf('The positive test result is not statistically significant (p = %.4f >= 0.05).\n', p_value);
-    end
-
-%% Bayesian approach:
-
-    % The concept of a p-value is less straightforward in Bayesian analysis. Bayesian analysis 
-    % focuses more on updating prior beliefs with new evidence and providing probabilities for 
-    % events rather than testing hypotheses with predefined significance levels. Bayesian analysis
-    % provides a probabilistic assessment of the strength of evidence for an event based on your 
-    % prior beliefs and the observed data. Whether a positive test result is considered significant 
-    % or not depends on the specific context and the threshold you set for your posterior 
-    % probabilities, but it's not directly comparable to the p<0.05 significance level used in 
-    % frequentist statistics.
-
-clc; clear;
-    % Define prior probability of being infected:
-    p_infected = 0.02; % 2% prior belief of infection
-
-    % Define likelihood of getting a positive test result if infected:
-    likelihood_infected = 1; % True positive rate
-    likelihood_uninfected = 0.05; % False positive rate
-
-    % Calculate posterior probability of being infected using Bayes' rule:
-    p_infected_pos = p_pos_infected * p_infected / (p_pos_infected ...
-    * p_infected + p_pos_uninfected * (1 - p_infected));
-
-    % Check if posterior probability is greater than 0.05:
-    if p_infected_pos > 0.05
-        fprintf('The positive test result is statistically significant (p = %.4f > 0.05).\n', p_infected_pos);
-    else
-        fprintf('The positive test result is not statistically significant (p = %.4f <= 0.05).\n', p_infected_pos);
-    end
+    % Yes, if the p-value is p<0.05, it suggests that the positive test 
+    % is "statistically significant". 
+    % 
+    % H:  When the test gets back a positive it means the person is infected
+    %     and when it gets a negative when the person is not infected.
+    % H0: The result of the test results negative or positive regardless of
+    %     the status of infection.
+    %
+    % If the p-value is p≥0.05 there is not enough evidence, so we reject the
+    % hypothesis (H) and accept the null hypothesis (H0). In other words, it 
+    % doesn't strongly support the claim that the person is infected or not 
+    % infected. Conversly, if the p-value is p<0.05 we reject the H0 and 
+    % accept the H. Meaning, that there is evidence to say that the positive
+    % or negative results reflects the status of infection. The p-value alone 
+    % doesn't tell you the probability of being infected; it tells you the 
+    % probability of observing the test result if the null hypothesis were true.
 
 %% Exercise #2: What is the probability that if someone gets a positive test, that person is infected?
+
 clc; clear;
-    % Define the range of priors (infection rates) from 0 to 1 in steps of 0.01:
-    prior_range = 0:0.01:1;
 
-    % Define likelihood of getting a positive test result if infected:
-    likelihood_infected = 1; % True positive rate (assuming perfect sensitivity)
-    likelihood_uninfected = 0.05; % False positive rate
+% Parameters
+num_samples = 1000;
+sensitivity = 0.95;  % Sensitivity of the test (true positive rate)
+specificity = 0.90;  % Specificity of the test (true negative rate)
 
-    % Iterate through each prior (infection rate):
-    for i = 1:length(prior_range);
-        % Calculate prior probability for current prior:
-        prior_p_infected = prior_range(i);
-        % Calculate posterior probability of being infected using Bayes' theorem:
-        posterior_p_infected = likelihood_infected * prior_p_infected / ...
-        (likelihood_infected * prior_p_infected + likelihood_uninfected * (1 - prior_p_infected));
-        % Store posterior probability:
-        posterior_p(i) = posterior_p_infected;
-    end
+% Initialize arrays to store results
+prior_probabilities = 0:0.01:1;  % Range of prior probabilities from 0 to 1 in steps of 0.1
+probabilities_infected_given_positive = zeros(size(prior_probabilities));
 
-    % Plot posterior probability as a function of prior probability:
-    figure;
-    plot(prior_range, posterior_p, 'LineWidth', 2);
-    xlabel('Prior probability of being infected');
-    ylabel('Posterior probability of being infected');
-    title('Posterior probability as a function of prior probability');
+% Loop through different prior probabilities
+for i = 1:length(prior_probabilities)
+    prior_probability = prior_probabilities(i);
 
-    % Calculate the probability that if someone gets a positive test, that person is infected:
-    p_infected_pos = likelihood_infected * prior_p_infected / ...
-        (likelihood_infected * prior_p_infected + likelihood_uninfected * (1 - prior_p_infected));
+    % Generate a random dataset of test results
+    test_results = rand(1, num_samples) < (prior_probability * sensitivity + (1 - prior_probability) * (1 - specificity));
 
-    % Print result:
-    fprintf('The probability that if someone gets a positive test, that person is infected is %.4f.\n', p_infected_pos);
+    % Calculate the probability of being infected given a positive test
+    P_positive_given_infected = sensitivity;  % Probability of testing positive given infected
+    P_infected = prior_probability;  % Prior probability of being infected
+    P_positive = sum(test_results) / num_samples;  % Probability of testing positive
 
-    
+    P_infected_given_positive = (P_positive_given_infected * P_infected) / P_positive;
 
+    % Store the result in the array
+    probabilities_infected_given_positive(i) = P_infected_given_positive;
+end
+
+% Display the results
+for i = 1:length(prior_probabilities)
+    fprintf('Prior Probability: %.4f, Probability of being infected given a positive test: %f\n', prior_probabilities(i), probabilities_infected_given_positive(i));
+end
+
+% Plot the results
+figure;
+plot(prior_probabilities, probabilities_infected_given_positive);
+xlabel('Prior Probability of Infection');
+ylabel('Probability of Being Infected Given a Positive Test');
+title('Probability vs. Prior Probability of Infection');
+grid on;
